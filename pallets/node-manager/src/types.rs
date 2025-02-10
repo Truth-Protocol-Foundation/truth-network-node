0a90779aaa25d460eded2ec4003d13585673aaff
nahuseyoum <nahu.seyoum@aventus.io>
2025-02-10 20:25:03 +0000

    SYS-4391 Reward period management

--- pallets/node-manager/src/types.rs
+++ pallets/node-manager/src/types.rs
@@ -1,6 +1,70 @@
 use crate::*;
 use sp_runtime::Saturating;
 
+#[derive(Copy, Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+/// The current era index and transition information
+pub struct RewardPeriodInfo<BlockNumber> {
+    /// Current era index
+    pub current: RewardPeriodIndex,
+    /// The first block of the current era
+    pub first: BlockNumber,
+    /// The length of the current era in number of blocks
+    pub length: u32,
+}
+
+impl<
+        B: Copy
+            + sp_std::ops::Add<Output = B>
+            + sp_std::ops::Sub<Output = B>
+            + From<u32>
+            + PartialOrd
+            + Saturating,
+    > RewardPeriodInfo<B>
+{
+    pub fn new(current: RewardPeriodIndex, first: B, length: u32) -> RewardPeriodInfo<B> {
+        RewardPeriodInfo { current, first, length }
+    }
+
+    /// Check if the reward period should be updated
+    pub fn should_update(&self, now: B) -> bool {
+        now.saturating_sub(self.first) >= self.length.into()
+    }
+
+    /// New reward period
+    pub fn update(&mut self, now: B) {
+        self.current = self.current.saturating_add(1u64);
+        self.first = now;
+    }
+}
+
+impl<
+        B: Copy
+            + sp_std::ops::Add<Output = B>
+            + sp_std::ops::Sub<Output = B>
+            + From<u32>
+            + PartialOrd
+            + Saturating,
+    > Default for RewardPeriodInfo<B>
+{
+    fn default() -> RewardPeriodInfo<B> {
+        RewardPeriodInfo::new(0u64, 0u32.into(), 20u32)
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+pub struct RewardPotInfo<Balance> {
+    /// The total reward to pay out
+    pub total_reward: Balance,
+    /// The total uptime for the reward period
+    pub total_uptime: u64,
+}
+
+impl<Balance: Copy> RewardPotInfo<Balance> {
+    pub fn new(total_reward: Balance, total_uptime: u64) -> RewardPotInfo<Balance> {
+        RewardPotInfo { total_reward, total_uptime }
+    }
+}
+
 #[derive(
     Copy, Clone, PartialEq, Default, Eq, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen,
 )]
@@ -38,5 +102,6 @@ impl<
 #[derive(Encode, Decode, TypeInfo, Debug, Clone, PartialEq)]
 pub enum AdminConfig<AccountId, Balance> {
     NodeRegistrar(AccountId),
+    RewardPeriod(u32),
     Heartbeat(u32),
 }
