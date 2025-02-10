eb0b0542fb0fca897ed2f378ba0cfe280f1f8e10
nahuseyoum <nahu.seyoum@aventus.io>
2025-02-10 19:45:07 +0000

    SYS-4389 Node registration logic

--- pallets/node-manager/src/lib.rs
+++ pallets/node-manager/src/lib.rs
@@ -26,6 +26,8 @@ use sp_runtime::{
     },
     DispatchError, RuntimeDebug, Saturating,
 };
+pub mod types;
+use crate::types::*;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
@@ -36,35 +38,130 @@ mod mock;
 #[cfg(not(feature = "std"))]
 use sp_std::prelude::*;
 
+const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+/// A type alias for a unique identifier of a node
+pub(crate) type NodeId<T> = <T as frame_system::Config>::AccountId;
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
+
     #[pallet::pallet]
     #[pallet::storage_version(STORAGE_VERSION)]
     pub struct Pallet<T>(_);
+
+    /// Map of registered nodes
+    #[pallet::storage]
+    pub type NodeRegistry<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        NodeId<T>,
+        NodeInfo<T::SignerId, T::AccountId>,
+        OptionQuery,
+    >;
+
+    // This is mainly used for performance reasons. It is better to have a single value storage than
+    // iterate over a huge map.
+    /// Total registered nodes.
+    #[pallet::storage]
+    pub type TotalRegisteredNodes<T: Config> = StorageValue<_, u32, ValueQuery>;
+
+    /// Registry of nodes with their owners.
+    #[pallet::storage]
+    pub type OwnedNodes<T: Config> = StorageDoubleMap<
+        _,
+        Blake2_128Concat,
+        T::AccountId, // OwnerAddress
+        Blake2_128Concat,
+        NodeId<T>,
+        (),
+        OptionQuery,
+    >;
+
+    /// The admin account that can register new nodes
+    #[pallet::storage]
+    pub type NodeRegistrar<T: Config> = StorageValue<_, T::AccountId, OptionQuery>;
 
     // Pallet Events
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        // Implement me
+        /// A new node has been registered
+        NodeRegistered { owner: T::AccountId, node: NodeId<T> },
+        /// A new node registrar has been set
+        NodeRegistrarSet { new_registrar: T::AccountId },
     }
 
     // Pallet Errors
     #[pallet::error]
     pub enum Error<T> {
-        // Implement me
+        /// The node registrar account is invalid
+        InvalidRegistrar,
+        /// The node registrar account is not set
+        RegistrarNotSet,
+        /// Node has already been registered
+        DuplicateNode,
     }
 
     #[pallet::config]
     pub trait Config:
         frame_system::Config + avn::Config + SendTransactionTypes<Call<Self>>
     {
-        // Implement me
+        /// The overarching event type.
+        type RuntimeEvent: From<Event<Self>>
+            + Into<<Self as frame_system::Config>::RuntimeEvent>
+            + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        /// The currency type for this module.
+        type Currency: Currency<Self::AccountId>;
+        // The identifier type for an offchain transaction signer.
+        type SignerId: Member
+            + Parameter
+            + RuntimeAppPublic
+            + Ord
+            + MaybeSerializeDeserialize
+            + MaxEncodedLen;
+        /// A type that can be used to verify signatures
+        type Public: IdentifyAccount<AccountId = Self::AccountId>;
+        /// The signature type used by accounts/transactions.
+        #[cfg(not(feature = "runtime-benchmarks"))]
+        type Signature: Verify<Signer = Self::Public> + Member + Decode + Encode + TypeInfo;
+
+        #[cfg(feature = "runtime-benchmarks")]
+        type Signature: Verify<Signer = Self::Public>
+            + Member
+            + Decode
+            + Encode
+            + TypeInfo
+            + From<sp_core::sr25519::Signature>;
+        /// The weight information for extrinsics in this pallet.
+        type WeightInfo: WeightInfo;
     }
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
+        /// Register a new node
+        #[pallet::call_index(0)]
+        #[pallet::weight(<T as Config>::WeightInfo::register_node())]
+        pub fn register_node(
+            origin: OriginFor<T>,
+            node: NodeId<T>,
+            owner: T::AccountId,
+            signing_key: T::SignerId,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+            let registrar = NodeRegistrar::<T>::get().ok_or(Error::<T>::RegistrarNotSet)?;
+            ensure!(who == registrar, Error::<T>::InvalidRegistrar);
+            ensure!(!<NodeRegistry<T>>::contains_key(&node), Error::<T>::DuplicateNode);
+
+            <OwnedNodes<T>>::insert(&owner, &node, ());
+            <NodeRegistry<T>>::insert(
+                &node,
+                NodeInfo::<T::SignerId, T::AccountId>::new(owner.clone(), signing_key),
+            );
+            <TotalRegisteredNodes<T>>::mutate(|n| *n = n.saturating_add(1));
+            Self::deposit_event(Event::NodeRegistered { owner, node });
+
+            Ok(())
+        }
         // Implement me
     }
 
--- pallets/node-manager/src/tests/mock.rs
+++ pallets/node-manager/src/tests/mock.rs
@@ -23,3 +23,71 @@ pub type AccountId = <Signature as Verify>::Signer;
 pub type Extrinsic = TestXt<RuntimeCall, ()>;
 
 type Block = frame_system::mocking::MockBlock<TestRuntime>;
+
+frame_support::construct_runtime!(
+    pub enum TestRuntime
+    {
+        System: frame_system::{Pallet, Call, Config<T>, Storage, Event<T>},
+        NodeManager: pallet_node_manager::{Pallet, Call, Storage, Event<T>, Config<T>},
+        AVN: pallet_avn::{Pallet, Storage, Event, Config<T>},
+    }
+);
+impl Config for TestRuntime {
+    type RuntimeEvent = RuntimeEvent;
+    type Currency = Balances;
+    type SignerId = UintAuthorityId;
+    type Public = AccountId;
+    type Signature = Signature;
+    type WeightInfo = ();
+}
+
+impl<LocalCall> system::offchain::SendTransactionTypes<LocalCall> for TestRuntime
+where
+    RuntimeCall: From<LocalCall>,
+{
+    type OverarchingCall = RuntimeCall;
+    type Extrinsic = Extrinsic;
+}
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub const MaximumBlockWeight: Weight = Weight::from_parts(1024 as u64, 0);
+    pub const MaximumBlockLength: u32 = 2 * 1024;
+    pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+    pub const ChallengePeriod: u64 = 2;
+}
+
+impl system::Config for TestRuntime {
+    type BaseCallFilter = frame_support::traits::Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type Nonce = u64;
+    type RuntimeCall = RuntimeCall;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Block = Block;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = BlockHashCount;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<u128>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
+    type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+impl pallet_avn::Config for TestRuntime {
+    type RuntimeEvent = RuntimeEvent;
+    type AuthorityId = UintAuthorityId;
+    type EthereumPublicKeyChecker = ();
+    type NewSessionHandler = ();
+    type DisabledValidatorChecker = ();
+    type WeightInfo = ();
+}
