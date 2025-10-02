6b90dc301a2e8eefd4097a23073687f228fb8b08
Nahu <39748285+nahuseyoum@users.noreply.github.com>
2025-10-02 21:21:09 +0100

    feat: adds unit tests (#78)

--- pallets/watchtower/src/proxy.rs
+++ pallets/watchtower/src/proxy.rs
@@ -14,6 +14,34 @@ impl<T: Config> Pallet<T> {
         (SIGNED_SUBMIT_EXTERNAL_PROPOSAL_CONTEXT, relayer.clone(), proposal, block_number).encode()
     }
 
+    // Voters and only vote once per proposal so no nonce needed here.
+    pub fn encode_signed_submit_vote_params(
+        relayer: &T::AccountId,
+        proposal_id: &ProposalId,
+        in_favor: &bool,
+        block_number: &BlockNumberFor<T>,
+    ) -> Vec<u8> {
+        (SIGNED_SUBMIT_VOTE_CONTEXT, relayer.clone(), proposal_id, in_favor, block_number).encode()
+    }
+
+    pub fn offchain_signature_is_valid<D: Encode>(
+        data: &D,
+        signer: &T::SignerId,
+        signature: &<T::SignerId as RuntimeAppPublic>::Signature,
+    ) -> bool {
+        let signature_valid =
+            data.using_encoded(|encoded_data| signer.verify(&encoded_data, &signature));
+
+        log::trace!(
+            "🪲 Validating ocw signature: [ data {:?} - account {:?} - signature {:?} ] Result: {}",
+            data.encode(),
+            signer.encode(),
+            signature,
+            signature_valid
+        );
+        return signature_valid;
+    }
+
     pub fn get_encoded_call_param(
         call: &<T as Config>::RuntimeCall,
     ) -> Option<(&Proof<T::Signature, T::AccountId>, Vec<u8>)> {
